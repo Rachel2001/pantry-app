d2a0cede99290050ca9665d8e65e1a42a1d0869a
Racheline <136965489+Rachel2001@users.noreply.github.com>
2024-08-01 16:43:46 +0200

    deleted all files, starting over

--- components/ui/background-gradient-animation.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function BackgroundGradientAnimation({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="background-animation">{children}</div>;
-}
